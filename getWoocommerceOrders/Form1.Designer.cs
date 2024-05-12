3e5eac787a693a4815de1009897e297cb332b236
sefadonmez <sefadonmez@hotmail.com>
2024-05-12 14:16:07 +0300

    Add project files.

---
+++ getWoocommerceOrders/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace getWoocommerceOrders
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewOrders = new System.Windows.Forms.DataGridView();
+            this.btnGetOrders = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBoxOrderDetails = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxOrderDetailsBilling = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxOrderDetailsShipping = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOrders)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridViewOrders
+            // 
+            this.dataGridViewOrders.AllowUserToAddRows = false;
+            this.dataGridViewOrders.AllowUserToDeleteRows = false;
+            this.dataGridViewOrders.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewOrders.Location = new System.Drawing.Point(13, 13);
+            this.dataGridViewOrders.Name = "dataGridViewOrders";
+            this.dataGridViewOrders.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dataGridViewOrders.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridViewOrders.Size = new System.Drawing.Size(1194, 170);
+            this.dataGridViewOrders.TabIndex = 0;
+            this.dataGridViewOrders.SelectionChanged += new System.EventHandler(this.dataGridViewOrders_SelectionChanged);
+            // 
+            // btnGetOrders
+            // 
+            this.btnGetOrders.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGetOrders.Location = new System.Drawing.Point(1112, 8);
+            this.btnGetOrders.Name = "btnGetOrders";
+            this.btnGetOrders.Size = new System.Drawing.Size(104, 52);
+            this.btnGetOrders.TabIndex = 1;
+            this.btnGetOrders.Text = "All Get Orders";
+            this.btnGetOrders.UseVisualStyleBackColor = true;
+            this.btnGetOrders.Click += new System.EventHandler(this.btnGetOrders_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.btnGetOrders);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 567);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1219, 72);
+            this.panel1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 38);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // richTextBoxOrderDetails
+            // 
+            this.richTextBoxOrderDetails.Location = new System.Drawing.Point(12, 211);
+            this.richTextBoxOrderDetails.Name = "richTextBoxOrderDetails";
+            this.richTextBoxOrderDetails.Size = new System.Drawing.Size(256, 189);
+            this.richTextBoxOrderDetails.TabIndex = 3;
+            this.richTextBoxOrderDetails.Text = "";
+            // 
+            // richTextBoxOrderDetailsBilling
+            // 
+            this.richTextBoxOrderDetailsBilling.Location = new System.Drawing.Point(274, 211);
+            this.richTextBoxOrderDetailsBilling.Name = "richTextBoxOrderDetailsBilling";
+            this.richTextBoxOrderDetailsBilling.Size = new System.Drawing.Size(256, 189);
+            this.richTextBoxOrderDetailsBilling.TabIndex = 4;
+            this.richTextBoxOrderDetailsBilling.Text = "";
+            // 
+            // richTextBoxOrderDetailsShipping
+            // 
+            this.richTextBoxOrderDetailsShipping.Location = new System.Drawing.Point(536, 211);
+            this.richTextBoxOrderDetailsShipping.Name = "richTextBoxOrderDetailsShipping";
+            this.richTextBoxOrderDetailsShipping.Size = new System.Drawing.Size(256, 189);
+            this.richTextBoxOrderDetailsShipping.TabIndex = 5;
+            this.richTextBoxOrderDetailsShipping.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1219, 639);
+            this.Controls.Add(this.richTextBoxOrderDetailsShipping);
+            this.Controls.Add(this.richTextBoxOrderDetailsBilling);
+            this.Controls.Add(this.richTextBoxOrderDetails);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridViewOrders);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOrders)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewOrders;
+        private System.Windows.Forms.Button btnGetOrders;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBoxOrderDetails;
+        private System.Windows.Forms.RichTextBox richTextBoxOrderDetailsBilling;
+        private System.Windows.Forms.RichTextBox richTextBoxOrderDetailsShipping;
+    }
+}
+
---
+++ getWoocommerceOrders/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+namespace getWoocommerceOrders
+{
+    public partial class Form1 : Form
+    {
+        private HttpClient client = new HttpClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+               }
+
+        private async void btnGetOrders_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string url = "https://website/wp-json/wc/v3/orders?consumer_key=URLKEY&consumer_secret=URLKEY";
+                // WooCommerce REST API'sinden tüm siparişleri çek
+                string responseBody = await client.GetStringAsync(url);
+
+                // Gelen JSON verisini DataGridView'e ekleme
+                var orders = JsonConvert.DeserializeObject<dynamic>(responseBody);
+                dataGridViewOrders.DataSource = orders;
+
+                MessageBox.Show("Siparişler başarıyla alındı ve DataGridView'e eklendi.", "Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Hata oluştu: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void dataGridViewOrders_SelectionChanged(object sender, EventArgs e)
+        {
+           
+                if (dataGridViewOrders.SelectedRows.Count > 0)
+                {
+                    dynamic selectedOrder = dataGridViewOrders.SelectedRows[0].DataBoundItem;
+
+                    richTextBoxOrderDetails.Clear();
+                    richTextBoxOrderDetails.AppendText($"ID: {selectedOrder.id}\n");
+                    richTextBoxOrderDetails.AppendText($"Parent ID: {selectedOrder.parent_id}\n");
+                    richTextBoxOrderDetails.AppendText($"Status: {selectedOrder.status}\n");
+                    richTextBoxOrderDetails.AppendText($"Currency: {selectedOrder.currency}\n");
+                    richTextBoxOrderDetails.AppendText($"Version: {selectedOrder.version}\n");
+                    richTextBoxOrderDetails.AppendText($"Prices Include Tax: {selectedOrder.prices_include_tax}\n");
+                    richTextBoxOrderDetails.AppendText($"Date Created: {selectedOrder.date_created}\n");
+                    richTextBoxOrderDetails.AppendText($"Date Modified: {selectedOrder.date_modified}\n");
+                    richTextBoxOrderDetails.AppendText($"Discount Total: {selectedOrder.discount_total}\n");
+                    richTextBoxOrderDetails.AppendText($"Discount Tax: {selectedOrder.discount_tax}\n");
+                    richTextBoxOrderDetails.AppendText($"Shipping Total: {selectedOrder.shipping_total}\n");
+                    richTextBoxOrderDetails.AppendText($"Shipping Tax: {selectedOrder.shipping_tax}\n");
+                    richTextBoxOrderDetails.AppendText($"Cart Tax: {selectedOrder.cart_tax}\n");
+                    richTextBoxOrderDetails.AppendText($"Total: {selectedOrder.total}\n");
+                    richTextBoxOrderDetails.AppendText($"Total Tax: {selectedOrder.total_tax}\n");
+                    richTextBoxOrderDetails.AppendText($"Customer ID: {selectedOrder.customer_id}\n");
+                    richTextBoxOrderDetails.AppendText($"order_key ID: {selectedOrder.order_key}\n");
+
+
+
+                //
+
+                richTextBoxOrderDetailsBilling.Clear();
+                richTextBoxOrderDetailsBilling.AppendText($"First Name: {selectedOrder.billing.first_name}\n");
+                richTextBoxOrderDetailsBilling.AppendText($"Last Name: {selectedOrder.billing.last_name}\n");
+                richTextBoxOrderDetailsBilling.AppendText($"Company: {selectedOrder.billing.company}\n");
+                richTextBoxOrderDetailsBilling.AppendText($"Address 1: {selectedOrder.billing.address_1}\n");
+                richTextBoxOrderDetailsBilling.AppendText($"Address 2: {selectedOrder.billing.address_2}\n");
+                richTextBoxOrderDetailsBilling.AppendText($"City: {selectedOrder.billing.city}\n");
+                richTextBoxOrderDetailsBilling.AppendText($"State: {selectedOrder.billing.state}\n");
+                richTextBoxOrderDetailsBilling.AppendText($"Postcode: {selectedOrder.billing.postcode}\n");
+                richTextBoxOrderDetailsBilling.AppendText($"Country: {selectedOrder.billing.country}\n");
+                richTextBoxOrderDetailsBilling.AppendText($"Email: {selectedOrder.billing.email}\n");
+                richTextBoxOrderDetailsBilling.AppendText($"Phone: {selectedOrder.billing.phone}\n");
+
+
+
+                //
+
+                richTextBoxOrderDetailsShipping.Clear();
+
+                richTextBoxOrderDetailsShipping.AppendText($"First Name: {selectedOrder.shipping.first_name}\n");
+                richTextBoxOrderDetailsShipping.AppendText($"Last Name: {selectedOrder.shipping.last_name}\n");
+                richTextBoxOrderDetailsShipping.AppendText($"Company: {selectedOrder.shipping.company}\n");
+                richTextBoxOrderDetailsShipping.AppendText($"Address 1: {selectedOrder.shipping.address_1}\n");
+                richTextBoxOrderDetailsShipping.AppendText($"Address 2: {selectedOrder.shipping.address_2}\n");
+                richTextBoxOrderDetailsShipping.AppendText($"City: {selectedOrder.shipping.city}\n");
+                richTextBoxOrderDetailsShipping.AppendText($"State: {selectedOrder.shipping.state}\n");
+                richTextBoxOrderDetailsShipping.AppendText($"Postcode: {selectedOrder.shipping.postcode}\n");
+                richTextBoxOrderDetailsShipping.AppendText($"Country: {selectedOrder.shipping.country}\n");
+                richTextBoxOrderDetailsShipping.AppendText($"Phone: {selectedOrder.shipping.phone}\n");
+
+            }
+
+
+        }
+    }
+}
